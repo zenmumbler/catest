baf930fb1e62d6eba9b060730c40f15aba414e94
Arthur Langereis <puurtuur@me.com>
2014-12-06 22:08:27 +0100

    Initial Commit

---
+++ catest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  catest
+//
+//  Created by Arthur Langereis on 2014/12/6.
+//  Copyright (c) 2014 logic-dream. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
